e3fc0efd03e0fed6e92a4b1f6e2a2840733200c2
Venky <venkatramanm@gmail.com>
2023-03-03 22:40:51 +0530

    Ondc Testing

--- src/main/java/in/succinct/onet/ondc/extensions/OndcApiAdaptor.java
+++ src/main/java/in/succinct/onet/ondc/extensions/OndcApiAdaptor.java
@@ -18,9 +18,7 @@ public class OndcApiAdaptor extends NetworkApiAdaptor {
 
     @Override
     public void select(CommerceAdaptor adaptor, Request request, Request reply) {
-        request.getMessage().setOrder(getInputOrder(adaptor,request.getMessage().getOrder()));
         new SearchAdaptor(adaptor).select(request,reply);
-        reply.getMessage().setOrder(getOutputOrder(adaptor,reply.getMessage().getOrder()));
     }
 
 
--- src/main/java/in/succinct/onet/ondc/extensions/OndcRegistry.java
+++ src/main/java/in/succinct/onet/ondc/extensions/OndcRegistry.java
@@ -4,12 +4,15 @@ import in.succinct.bpp.core.adaptor.NetworkAdaptor;
 import in.succinct.bpp.core.adaptor.api.NetworkApiAdaptor;
 
 public class OndcRegistry extends NetworkAdaptor {
+    protected OndcRegistry(){
 
-    public OndcRegistry(String networkName){
+    }
+
+    protected OndcRegistry(String networkName){
         super(networkName);
     }
 
-    private final OndcApiAdaptor networkApiAdaptor = new OndcApiAdaptor(this);
+    private final transient OndcApiAdaptor networkApiAdaptor = new OndcApiAdaptor(this);
     @Override
     public NetworkApiAdaptor getApiAdaptor() {
         return networkApiAdaptor;
