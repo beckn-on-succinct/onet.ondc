8e75234e573283b128d44944ab698f11f34cdb13
Venky <venkatramanm@gmail.com>
2023-11-21 22:18:45 +0530

    ondc fixes

--- src/main/java/in/succinct/beckn/ondc/retail/Provider.java
+++ src/main/java/in/succinct/beckn/ondc/retail/Provider.java
@@ -1,5 +1,7 @@
 package in.succinct.beckn.ondc.retail;
 
+import in.succinct.beckn.Categories;
+
 public class Provider extends in.succinct.beckn.Provider {
     @Override
     public boolean isExtendedAttributesDisplayed(){
@@ -32,4 +34,11 @@ public class Provider extends in.succinct.beckn.Provider {
         set("tags",tags);
     }
 
+
+    public Categories getCategories(){
+        return super.getCategories();
+    }
+    public void setCategories(Categories categories){
+        rm("categories"); //Ondc does not want categories unless there is a customization enabled for that category.
+    }
 }
